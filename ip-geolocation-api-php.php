9fb366027bff422ab499513f95e1de31186827be
irtaza <irtaza@jfreaks.com>
2018-08-03 20:56:21 +0500

    add variable instead of dummy key

--- ip-geolocation-api-php.php
+++ ip-geolocation-api-php.php
@@ -1,7 +1,7 @@
 
 <?php
 
-$response = get_web_page("https://api.ipgeolocation.io/ipgeo?apiKey=d9d46f08bfb34ee59e4e8c74f07bb449&ip=103.255.5.105");
+$response = get_web_page("https://api.ipgeolocation.io/ipgeo?apiKey=YOUR_API_KEY&ip=103.255.5.105");
 $resArr = array();
 $resArr = json_decode($response);
 echo "<pre>"; print_r($resArr); echo "</pre>";
