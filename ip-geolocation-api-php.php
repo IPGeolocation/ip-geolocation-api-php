f68b86408690e1106dd5d502e5e2747fdc342c58
irtaza100 <34660777+irtaza100@users.noreply.github.com>
2018-08-13 23:13:32 +0500

    Update ip-geolocation-api-php.php

--- ip-geolocation-api-php.php
+++ ip-geolocation-api-php.php
@@ -1,25 +1,26 @@
-
 <?php
+$clientIp = "YOUR_CLIENT_IP";
+$ipkey = "YOUR_API_KEY";
 
-$response = get_web_page("https://api.ipgeolocation.io/ipgeo?apiKey=YOUR_API_KEY&ip=103.255.5.105");
+$response = get_location($ipkey, $clientIp);
 $resArr = array();
 $resArr = json_decode($response);
 echo "<pre>"; print_r($resArr); echo "</pre>";
-
-function get_web_page($url) {
-    $cURL = curl_init();
-    curl_setopt($cURL, CURLOPT_URL, $url);
-    curl_setopt($cURL, CURLOPT_HTTPGET, true);
+function get_location($apiKey, $ip = null) {
+ $url = "https://api.ipgeolocation.io/ipgeo?apiKey=".$apiKey."&ip=".$ip;
+ $cURL = curl_init();
+ curl_setopt($cURL, CURLOPT_URL, $url);
+ curl_setopt($cURL, CURLOPT_HTTPGET, true);
 
 curl_setopt($cURL, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-    'Accept: application/json'
+ 'Content-Type: application/json',
+ 'Accept: application/json'
 ));
 
-    $content  = curl_exec($cURL);
+ $content = curl_exec($cURL);
 
-    curl_close($cURL);
+ curl_close($cURL);
 
-    return $content;
+ return $content;
 }
 ?>
