3396361ef52bd8a5c7f7dbeac76c9f5b8b465096
Usman Liaqat <7531531+usmanbinliaqat@users.noreply.github.com>
2018-09-12 18:59:47 +0500

    Content didn't contain anything

--- ip-geolocation-api-php.php
+++ ip-geolocation-api-php.php
@@ -1,24 +1,25 @@
 <?php
-    $clientIp = "YOUR_CLIENT_IP";
-    $apikey = "YOUR_API_KEY";
+    $clientIp = "CLIENT_IP_ADDRESS";
+    $apikey = "PUT_YOUR_API_KEY_HERE";
     $response = get_location($apikey, $clientIp);
-    $resArr = array();
-    $resArr = json_decode($response);
-    echo "<pre>"; print_r($resArr); echo "</pre>";
+    $json = array();
+    $json = json_decode($response, true);
     
+    echo "<pre>";
+    print_r($json);
+    echo "</pre>";
+
     function get_location($apiKey, $ip = null) {
         $url = "https://api.ipgeolocation.io/ipgeo?apiKey=".$apiKey."&ip=".$ip;
         $cURL = curl_init();
-        
+
         curl_setopt($cURL, CURLOPT_URL, $url);
         curl_setopt($cURL, CURLOPT_HTTPGET, true);
+        curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($cURL, CURLOPT_HTTPHEADER, array(
             'Content-Type: application/json',
             'Accept: application/json'
         ));
-        
-        $content = curl_exec($cURL);
-        curl_close($cURL);
-        return $content;
+        return curl_exec($cURL);
     }
 ?>
